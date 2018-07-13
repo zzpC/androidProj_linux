b29ea421d340f73571629d3c5a9cc86e0496606b
zzpC <a1348778498@outlook.com>
2018-07-14 07:19:50 +0800

    .

--- Animator_PracticeDraw6/PracticeDraw6/app/src/main/java/com/hencoder/hencoderpracticedraw6/Utils.java
+++ Animator_PracticeDraw6/PracticeDraw6/app/src/main/java/com/hencoder/hencoderpracticedraw6/Utils.java
@@ -5,6 +5,21 @@ import android.util.DisplayMetrics;
 
 public class Utils {
     public static float dpToPixel(float dp) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         return dp * metrics.density;
     }
